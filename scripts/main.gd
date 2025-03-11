c803c92bf39bf107bdd93d85d2380c8f5eafcc90
Sam <46286293+fireproofpenguin@users.noreply.github.com>
2025-03-11 01:23:10 +0000

    Added gameover UI, connected gameover (not working)

--- scripts/main.gd
+++ scripts/main.gd
@@ -2,3 +2,16 @@ extends Node2D
 
 @export var bird: Bird
 @export var background: TileMapLayer
+
+@export var grass_animation: AnimationPlayer
+@export var cloud_animation: AnimationPlayer
+@export var building_animation: AnimationPlayer
+
+func _ready():
+	bird.on_death.connect(_game_over)
+
+func _game_over():
+	print("Game over")
+	grass_animation.pause()
+	cloud_animation.pause()
+	building_animation.pause()
